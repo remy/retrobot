0ccd1f50c38dc8454c2ce7a9f2e7ea26a4f26fdc
Anton Drukh <adrukh@users.noreply.github.com>
2016-11-16 10:58:04 +0200

    fix: typos

--- scripts/travis_after_success.sh
+++ scripts/travis_after_success.sh
@@ -5,16 +5,16 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "Pull request not trigerring deploy..."
   exit 0
 fi
+
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
-  echo "Deploying ${COMMIT_HASH} on ${DEPLOY_ENV} env"
+  export COMMIT_MESSAGE=`git log -n 1 --format='commit %h %s'`
+  export COMMIT_HASH=${TRAVIS_COMMIT}
+  
+  echo "Deploying ${COMMIT_HASH} on dev env"
   curl -i -H "Accept: application/json" -H "Content-Type: application/json" \
        -X POST -d "{\"commit_msg\":\"${COMMIT_MESSAGE}\", \
                     \"commit_hash\":\"${COMMIT_HASH}\"}" \
        https://my.dev.snyk.io/${DEPLOY_TOKEN}
 else
   echo "Building branch ${TRAVIS_BRANCH}, not trigerring deploy"
-  exit 0
 fi
-
-export COMMIT_MESSAGE=`git log -n 1 --format='commit %h %s'`
-export COMMIT_HASH=${TRAVIS_COMMIT}
